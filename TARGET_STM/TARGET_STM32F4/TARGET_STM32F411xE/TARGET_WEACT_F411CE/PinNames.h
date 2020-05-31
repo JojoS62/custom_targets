b821e6a2310bebfe7a460b5d3faf2865f5e8b2b0
JojoS <sn-mail@onlinehome.de>
2020-05-31 16:39:07 +0200

    update for F411 WeAct

--- TARGET_STM/TARGET_STM32F4/TARGET_STM32F411xE/TARGET_WEACT_F411CE/PinNames.h
+++ TARGET_STM/TARGET_STM32F4/TARGET_STM32F411xE/TARGET_WEACT_F411CE/PinNames.h
@@ -158,12 +158,12 @@ typedef enum {
 #ifdef MBED_CONF_TARGET_STDIO_UART_TX
     STDIO_UART_TX = MBED_CONF_TARGET_STDIO_UART_TX,
 #else
-    STDIO_UART_TX = PA_9,
+    STDIO_UART_TX = PA_2,
 #endif
 #ifdef MBED_CONF_TARGET_STDIO_UART_RX
     STDIO_UART_RX = MBED_CONF_TARGET_STDIO_UART_RX,
 #else
-    STDIO_UART_RX = PA_10,
+    STDIO_UART_RX = PA_3,
 #endif
 
     // Generic signals namings
