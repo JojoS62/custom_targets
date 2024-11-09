ab525190383fb0c088bd14e5c3e9e6d4d69e6c1a
Johannes Stratmann <sn2010@onlinehome.de>
2024-11-09 14:19:07 +0100

    add fmc init

---
+++ TARGET_STM32H7/TARGET_FK743M2_IIT6/fmc.h
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * File Name          : FMC.h
+  * Description        : This file provides code for the configuration
+  *                      of the FMC peripheral.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+#ifndef __FMC_H
+#define __FMC_H
+
+#include "mbed.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern SDRAM_HandleTypeDef hsdram1;
+
+void MX_FMC_Init(void);
+void HAL_SDRAM_MspInit(SDRAM_HandleTypeDef* hsdram);
+void HAL_SDRAM_MspDeInit(SDRAM_HandleTypeDef* hsdram);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__FMC_H */
